1addb61f7e76909c6125be2a2051bd305d28c538
Ali Hasanov <68409324+hasan0v@users.noreply.github.com>
2024-09-04 06:14:57 +0400

    Update settings.py

--- Irrigo/settings.py
+++ Irrigo/settings.py
@@ -142,14 +142,10 @@ CHANNEL_LAYERS = {
 
 
 MEDIA_URL = 'media/'
-# MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-]
 STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
 
 
